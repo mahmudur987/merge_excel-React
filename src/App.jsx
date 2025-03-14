d7ee25833238f21d89624f29fe8b308b5c27e71a
mahmudur987 <safemahmud987@gmail.com>
2025-03-14 07:02:56 +0600

    add gpo2c filter

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,12 @@ function App() {
           <h1 className="text-3xl text-center uppercase font-bold py-10">
             You can merge Your csv , xls, xlsx files here
           </h1>
-
+          <a
+            href="../GPO2CFILTER.html"
+            className="fixed top-5 right-20 font-semibold text-red-500"
+          >
+            GP O2C Filter
+          </a>
           <div>
             <FileMerger />
           </div>
