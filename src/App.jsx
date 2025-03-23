44498f7fcba3b26c6575b99d5ad0ced5e28f1865
mahmudur987 <108387570+mahmudur987@users.noreply.github.com>
2025-03-23 10:35:56 +0600

    gp o2c filter done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Toaster } from "react-hot-toast";
 import FileMerger from "./component/FileMerger";
+import ExcelResellerFilter from "./component/GPO2CFILTER";
 
 function App() {
   return (
     <main className="bg-black">
       <section className="container bg-purple-300 min-h-screen mx-auto">
         <header>
-          <h1 className="text-3xl text-center uppercase font-bold py-10">
+          {/* <h1 className="text-3xl text-center uppercase font-bold py-10">
             You can merge Your csv , xls, xlsx files here
           </h1>
           <a
@@ -17,7 +18,9 @@ function App() {
           </a>
           <div>
             <FileMerger />
-          </div>
+          </div> */}
+
+          <ExcelResellerFilter />
         </header>
       </section>
       <Toaster />
